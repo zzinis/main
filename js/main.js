f11ce8024a710ac02a616bb8ad90688e864b514a
zzinis <lizzyin@naver.com>
2021-11-07 15:16:08 +0900

    main btnview video 추가

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const next = $(".btnNext");
 const txt = $("#txt ul");
 const txt_li = txt.children("li");
 const sub_txt = $("#subTxt ul");
+const bg = $(".bg ul");
 
 const img = $("#pic ul");
 const num = $("#num ul");
@@ -19,7 +20,7 @@ sub_txt.find("li").eq(1).addClass("on");
 img.find("li").eq(1).addClass("on");
 
 // next
-next.on("click",e=>{
+next.on("click",function(e){
     e.preventDefault();
 
     panel.find("li").removeClass("on");
@@ -32,7 +33,7 @@ next.on("click",e=>{
 
     txt.find("li").removeClass("on");
     
-    txt.stop().animate({top: "-200%"}, speed, ()=>{
+    txt.stop().animate({top: "-200%"}, 400, ()=>{
         txt.find("li").first().appendTo(txt);
         txt.css({top: "-100%"});
         txt.find("li").eq(1).addClass("on");
@@ -40,30 +41,38 @@ next.on("click",e=>{
 
     sub_txt.find("li").removeClass("on");
     
-    sub_txt.stop().animate({top: "-200%"}, speed, ()=>{
+    sub_txt.stop().animate({left: "-100%"}, 700, ()=>{
         sub_txt.find("li").first().appendTo(sub_txt);
-        sub_txt.css({top: "-100%"});
+        sub_txt.css({left: "0%"});
         sub_txt.find("li").eq(1).addClass("on");
     });
 
+    bg.find("li").removeClass("on");
+
+    bg.stop().animate({left:"-100%"}, 400, ()=>{
+        bg.find("li").first().appendTo(bg);
+        bg.css({left: "0%"});
+        bg.find("li").eq(1).addClass("on");
+    })
+
     img.find("li").removeClass("on");
 
-    img.stop().animate({top: "-200%"}, speed, ()=>{
+    img.stop().animate({left: "-100%"}, speed, ()=>{
         img.find("li").first().appendTo(img);
-        img.css({top: "-100%"});
+        img.css({left: "0%"});
         img.find("li").eq(1).addClass("on");
     });
 
-    num.stop().animate({marginRight: "-200%"}, speed, ()=>{
+    num.stop().animate({right: "-100%"}, speed, ()=>{
         num.find("li").first().appendTo(num);
-        num.css({marginRight: "-100%"});
+        num.css({right: "-0%"});
         num.find("li").eq(1).addClass("on");
     });
 
 });
 
 // prev
-prev.on("click",e=>{
+prev.on("click",function(e){
     e.preventDefault();
 
     panel.find("li").removeClass("on");
@@ -76,7 +85,7 @@ prev.on("click",e=>{
 
     txt.find("li").removeClass("on");
 
-    txt.stop().animate({top: "0%"}, speed, ()=>{
+    txt.stop().animate({top: "0%"}, 400, ()=>{
         txt.find("li").last().prependTo(txt);
         txt.css({top: "-100%"});
         txt.find("li").eq(1).addClass("on");
@@ -84,23 +93,87 @@ prev.on("click",e=>{
 
     sub_txt.find("li").removeClass("on");
 
-    sub_txt.stop().animate({top: "0%"}, speed, ()=>{
+    sub_txt.stop().animate({left: "-200%"}, 700, ()=>{
         sub_txt.find("li").last().prependTo(sub_txt);
-        sub_txt.css({top: "-100%"});
+        sub_txt.css({left: "0%"});
         sub_txt.find("li").eq(1).addClass("on");
     });
 
+    bg.find("li").removeClass("on");
+
+    bg.stop().animate({left:"100%"}, 400, ()=>{
+        bg.find("li").first().appendTo(bg);
+        bg.css({left: "0%"});
+        bg.find("li").eq(1).addClass("on");
+    })
+
     img.find("li").removeClass("on");
 
-    img.stop().animate({top: "0%"}, speed, ()=>{
+    img.stop().animate({left: "100%"}, speed, ()=>{
         img.find("li").last().prependTo(img);
-        img.css({top: "-100%"});
+        img.css({left: "0%"});
         img.find("li").eq(1).addClass("on");
     });
 
-    num.stop().animate({marginRight: "0%"}, speed, ()=>{
+
+    num.stop().animate({marginRight: "100%"}, speed, ()=>{
         num.find("li").last().prependTo(num);
-        num.css({marginRight: "-100%"});
+        num.css({marginRight: "0%"});
         num.find("li").eq(1).addClass("on");
     });
+});
+
+
+// $("body").on("click", ".btnview", function(e){
+//     e.preventDefault();
+
+//     let imgSrc = $(this).children("video");
+//     // var video = $('<video />', {
+//     //     id: 'video',
+//     //     src: './img/interior.mp4',
+//     //     type: 'video/mp4',
+//     //     controls: true
+//     // });
+//     video.appendTo($('#video'));
+//     var video = $('<video width="640" height="264" autoplay></video>')
+//             .append('<source src="./img/interior.mp4" type="video/mp4" />')
+//             .appendTo($("#video"));
+
+//     $("body").append(
+//         $("<div class = 'pop'>")
+//             .append(
+//                 $("<video>"),
+//                 $("<span>").text("close")
+//             )
+//     )
+// });
+
+// $("body").on("click", ".pop span", function(){
+//     $(".pop").fadeOut(500,function(){
+//         $(this).remove();
+//     })
+// });
+
+
+$("body").on("click", ".btnview", function(e){
+    e.preventDefault();
+
+    let imgSrc = $(this).children("a").attr("href");
+    var video = $('<video width="640" height="264" autoplay></video>')
+            .append('<source src="./img/interior.mp4" type="video/mp4" />')
+            .appendTo($("#video"));
+
+    $("body").append(
+        $("<div class = 'pop'>")
+            .append(
+                $("<video>").attr({src: imgSrc}),
+                $("<span>").text("close")
+            )
+    )
+});
+
+$("body").on("click", ".pop span", function(){
+    $(".pop").fadeOut(500,function(){
+        $(this).remove();
+    })
 });
